a92fe550281a905aa5a0e007197aa46e846ebe1a
thelegend2004 <oleh.liubichenko@gmail.com>
2025-07-16 20:46:39 +0300

    fix: fix randy hover

--- src/components/Randy.tsx
+++ src/components/Randy.tsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 import RandyIcon from "../assets/randy.svg?react";
 import RandyCloseIcon from "../assets/randyClose.svg?react";
+import RandyTalkIcon from "../assets/randyTalk.svg?react";
 
 export function Randy() {
   const [isHovered, setIsHovered] = useState(false);
   return (
-    <div
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      className="inline-block"
-    >
-      {isHovered ? (
-        <RandyCloseIcon className="inline h-24 w-24 mx-1" />
-      ) : (
-        <RandyIcon className="inline h-24 w-24 mx-1" />
-      )}
+    <div className="inline-block">
+      <div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {isHovered ? (
+          <RandyCloseIcon className="inline h-24 w-24 mx-1" />
+        ) : (
+          <RandyIcon className="inline h-24 w-24 mx-1" />
+        )}
+      </div>
     </div>
   );
 }
