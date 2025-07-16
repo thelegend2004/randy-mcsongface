f4769750e86cdbc7f7d49cd86c75cda50c959ecf
thelegend2004 <oleh.liubichenko@gmail.com>
2025-07-16 18:22:20 +0300

    fix: minor vercel fixes

--- api/deezer-search.js
+++ api/deezer-search.js
@@ -1,4 +1,4 @@
-export default async function handler(req: any, res: any) {
+export default async function handler(req, res) {
   const { searchParams } = new URL(req.url, `http://${req.headers.host}`);
   const query = searchParams.get("q");
 
